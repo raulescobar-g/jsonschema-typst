f19e6f0655d07cbf2b2a8d5df8b213fe021a1b58
Raul Escobar <email@raulesco.bar>
2025-10-30 14:49:19 -0500

    renamed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh 
 
 RUSTFLAGS='--cfg getrandom_backend="custom"' cargo build --release --target wasm32-unknown-unknown;
-cp ./target/wasm32-unknown-unknown/release/jsonschema_typst.wasm ./typst-package/jsonschema.wasm
+cp ./target/wasm32-unknown-unknown/release/jsonschemeyst.wasm ./typst-package/jsonschemeyst.wasm
 
 cp README.md ./typst-package/README.md
 
